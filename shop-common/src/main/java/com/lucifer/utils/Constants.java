75e5f62ad10cd57f0d4a049c013b28a75e7bb44a
Wang Di <wangdi@xiaobangtouzi.com>
2019-04-30 00:48:55 +0800

    init

---
+++ shop-common/src/main/java/com/lucifer/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.lucifer.utils;
+
+/**
+ * @Author : wangdi
+ * @Date : creat in 2019/4/29 23:56
+ * 常量类
+ */
+public class Constants {
+}
---
+++ src/main/java/com/lucifer/config/Swagger2.java
@@ -0,0 +1,43 @@
+package com.lucifer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author : wangdi
+ * @Date : creat in 2019/4/29 23:57
+ */
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //选择controller包
+                .apis(RequestHandlerSelectors.basePackage("com.lucifer.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //自定义信息可按需求填写
+                .title("Spring Boot中使用Swagger构建RESTful APIs")
+                .description("测试")
+                .termsOfServiceUrl("http://www.duanxiaowei.top")
+                .contact("刘林林")
+                .version("1.0")
+                .build();
+    }
+}
